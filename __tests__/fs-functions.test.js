9348cca87f11dd46a5922dad6fd93496d696d4e6
drmeloy <drmeloy@gmail.com>
2019-12-04 13:59:37 -0700

    mkdirp and writeJSON tests and functions written

--- __tests__/fs-functions.test.js
+++ __tests__/fs-functions.test.js
@@ -5,17 +5,42 @@
 // updateJSON - update a files JSON
 // deleteFile - delete a file
 
-const js = require('js').promises;
-const { mkdirp } = require('../lib/fs-functions');
+const fs = require('fs').promises;
+const { mkdirp, writeJSON } = require('../lib/fs-functions');
 
-jest.mock('js', () => ({
+jest.mock('fs', () => ({
   promises: {
-    
+    mkdir: jest.fn(() => Promise.resolve()),
+    writeFile: jest.fn(() => Promise.resolve())
   }
 }));
 
 describe('mkdirp', () => {
-  it('should make a directory and all parent directories', () => {
+  it('should make a directory and all parent directories', () => { return mkdirp('./test/file/rad')
+    .then(() => {
+      expect(fs.mkdir).toHaveBeenLastCalledWith('./test/file/rad', { recursive: true });
+    });
+  });
+
+  it('should specify if file already exists', () => {
 
   });
+});
+
+describe('writeJSON', () => {
+  const dog = {
+    name: 'Rover',
+    age: 200
+  };
+
+  it('should write an object to a file', () => {
+    return writeJSON(dog, './test/file/rad')
+      .then(() => {
+        expect(fs.writeFile).toHaveBeenLastCalledWith(dog, './test/file/rad');
+      });
+  });
+
+  if('should throw error if file does not exist', () => {
+    
+  })
 });
